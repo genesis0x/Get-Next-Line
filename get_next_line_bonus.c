b3db522a6ded4b5204d00a8d4f7535833e7d682f
Hamza Hadioui <hahadiou@c1r1p1.1337.ma>
2022-11-06 21:36:18 +0100

    Save Project

---
+++ get_next_line_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hahadiou <hahadiou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 21:41:47 by hahadiou          #+#    #+#             */
+/*   Updated: 2022/11/03 14:57:32 by hahadiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*realloc_line_buffer(t_line_buffer *pl)
+{
+	char	*new_buf;
+	size_t	new_size;
+
+	new_size = pl->size + 1;
+	new_buf = malloc(sizeof(char) * (new_size + 1));
+	if (pl->buf)
+	{
+		if (new_buf)
+			ft_memcpy(new_buf, pl->buf, pl->size);
+		free(pl->buf);
+	}
+	pl->buf = new_buf;
+	pl->size = new_size;
+	return (new_buf);
+}
+
+static char	*alloc_read_buffer(t_read_buffer *pb)
+{
+	if (pb->buf == 0)
+	{
+		pb->buf = malloc(sizeof(char) * BUFFER_SIZE);
+		if (pb->buf == 0)
+			return (0);
+		pb->pos = 0;
+		pb->size = 0;
+	}
+	return (pb->buf);
+}
+
+static int	read_file(int fd, t_read_buffer *rb)
+{
+	if (rb->pos >= rb->size)
+	{
+		rb->size = read(fd, rb->buf, BUFFER_SIZE);
+		if (rb->size < 1)
+			return (0);
+		rb->pos = 0;
+	}
+	return (rb->buf[rb->pos++]);
+}
+
+static int	put_line_char(t_line_buffer *pl, char c)
+{
+	if (pl->pos >= pl->size)
+	{
+		if (realloc_line_buffer(pl) == 0)
+			return (0);
+	}
+	pl->buf[pl->pos++] = c;
+	if (c == '\n')
+		return (0);
+	return (1);
+}
+
+char	*get_next_line(int fd)
+{
+	int						c;
+	t_line_buffer			line;
+	static t_read_buffer	rb[BUFFER_SIZE];
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line.buf, 0) < 0)
+		return (0);
+	if (alloc_read_buffer(&rb[fd]) == 0)
+		return (0);
+	line.buf = 0;
+	line.pos = 0;
+	line.size = 0;
+	while (1)
+	{
+		c = read_file(fd, &rb[fd]);
+		if (c == 0)
+			break ;
+		if (put_line_char(&line, c) == 0)
+			break ;
+	}
+	if (line.pos == 0)
+		free_read_buffer(&rb[fd]);
+	else
+		line.buf[line.pos] = 0;
+	return (line.buf);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hahadiou <hahadiou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 14:56:47 by hahadiou          #+#    #+#             */
+/*   Updated: 2022/11/04 12:35:59 by hahadiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stdio.h>
+# include <limits.h>
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 256
+# endif
+
+#ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+#endif
+
+typedef struct s_buffer
+{
+	char	*buf;
+	size_t	pos;
+	size_t	size;
+}	t_buffer;
+
+typedef t_buffer	t_line_buffer;
+typedef t_buffer	t_read_buffer;
+
+char	*get_next_line(int fd);
+// char *gnl(int fd);
+// int BBN(char *s, char c);
+// char *join(char *s1, char *s2);
+// char *strjoin_li_makadir_walo(char *s1, char *s2);
+// char *ft_read_file(int fd, char *ret);
+// int ft_strchr(char *s, int c);
+// char *ft_strjoin(char  *s1, char *s2);
+// char *ft_get_line(char *buffer);
+// char *ft_get_next_line(char *buffer);
+// int ft_strlen(char *s);
+// void	*ft_calloc(size_t count, size_t size);
+// void	*ft_memset(void *b, int c, size_t len);
+// void	ft_bzero(void *s, size_t n);
+// char *ft_join_and_free(char *ret, char *buffer);
+// char *gnl(int fd);
+// char *ft_get_next_line_v3(int fd);
+// void ft_saw(char *s);
+void	*ft_memcpy(void	*dst, const void	*src, size_t	n);
+void	free_read_buffer(t_read_buffer *prb);
+
+#endif
